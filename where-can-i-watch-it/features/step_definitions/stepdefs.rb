dc4622e7a9e869652039c0b76829eddcc73ac01b
Will <wmorris1367@gmail.com>
2019-03-15 14:05:52 -0700

    added the test templates in the stepdefs file

--- where-can-i-watch-it/features/step_definitions/stepdefs.rb
+++ where-can-i-watch-it/features/step_definitions/stepdefs.rb
@@ -25,3 +25,76 @@ end
 Then("the user is granted access into the account") do
 	'Logged in successfully'
 end
+
+Given("the username is not already taken") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("the user enters a username for new account") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the account is created") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("the username is already taken") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the account is not created") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("the user password is correct") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("the user enters in the correct password") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the user is able to login") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("the user password is not correct") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("the user enters in the wrong password") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the user is not able to login") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+
+
+# Search Tests
+module SearchStepHelper
+	def search
+		
+	end
+end
+Given("the movie can be found in the Utelly API") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("the searches for the movie") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the user should see the movies details.") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("the movie cannot be found in the Utelly API") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the user should no results.") do
+	pending # Write code here that turns the phrase above into concrete actions
+end
+
