e3dad872e84254c1f9423ef8721fb187db62ad07
leadinmyhead <cdavis5@uccs.edu>
2019-04-06 01:28:57 +0000

    favorites step defs file

---
+++ where-can-i-watch-it/features/step_definitions/favorites.rb
@@ -0,0 +1,25 @@
+
+
+#   Scenario: the user adds a movie to their favorites list
+Given("the user is on the movies page") do
+end
+When("the user presses the 'add to favorites button'") do
+end
+Then("the movie is added to their favorites list") do
+end
+    
+#   Scenario: the user removes a movie to their favorites list
+Given("the is on the favorites page") do
+end
+When("the user presses the 'remove from favorites button'") do
+end
+Then("the movie is removed from their favorites list") do
+end   
+
+#   Scenario: the user adds a movie to their favorites list, but is not logged in
+Given("the is on the movies page") do
+end
+When("the user press the 'add to favorites button'") do
+end
+Then("the movie is not added to their favorites list, and are requested to log in") do
+end
