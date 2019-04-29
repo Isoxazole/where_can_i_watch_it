148377d6643edd2737325262d98c6c8c90971ff7
leadinmyhead <Xo34Xo1@gmail.com>
2019-04-29 04:52:09 +0000

    filter tests complete

--- where-can-i-watch-it/features/filter.feature
+++ where-can-i-watch-it/features/filter.feature
@@ -1,14 +1,12 @@
 Feature: Filter
     A feature to allow a user to filter movies shown on the homepage by provider
     
-    
 Scenario: the user selects to filter by a provider 
-    Given the user is on the movies page
-    When the user selects what provider to filter by and presses "filter"
+    Given the user is in the main movies page
+    When the user selects what provider to filter by and presses 'filter'
     Then the movies displayed should only belong to selected providers
     
-Scenario: the user selects to "Clear"
-    Given the user is on the movies page
-    When the user selects what provider to filter by and presses "Clear" instead
+Scenario: the user selects to 'Clear'
+    Given the user is located in the main movies page
+    When the user selects what provider to filter by and presses 'Clear' instead
     Then all movies should be displayed
-
---
+++ where-can-i-watch-it/features/step_definitions/filter.rb
@@ -0,0 +1,45 @@
+require 'cucumber/rails'
+require 'capybara'
+require 'rubygems'
+
+module FilterStepHelper
+
+	def load_database
+	    load File.join(Rails.root, 'db', 'seeds.rb')
+	end   
+	
+end
+
+World FilterStepHelper
+
+#Scenario: the user selects to filter by a provider 
+    Given ("the user is in the main movies page") do
+        load_database
+        visit "/"
+    end
+    When ("the user selects what provider to filter by and presses 'filter'") do
+        check('ratings[Netflix]')
+        click_button "Refresh"
+    end
+    Then ("the movies displayed should only belong to selected providers") do
+        page.should_not have_content("Mother!")
+        page.should_not have_content("A Quite Place")
+        page.should_not have_content("Holmes and Watson")
+    end
+    
+#Scenario: the user selects to 'Clear'
+    Given ("the user is located in the main movies page") do
+        visit "/"
+    end
+    When ("the user selects what provider to filter by and presses 'Clear' instead") do
+        check('ratings[Netflix]')
+        click_button "Refresh"
+        click_button "Clear"
+    end
+    Then ("all movies should be displayed") do
+        page.should have_content("Apostle")
+        page.should have_content("Mother!")
+        page.should have_content("A Quiet Place")
+        page.should have_content("Holmes and Watson")
+    end
+    
